64018f549873023c0592979f4d667ef77d300a2b
amalijayawardhana <iasandaruwani@gmail.com>
2020-07-27 18:29:12 +0530

    add businessLogic file

--- src/business/BusinessLogic.java
+++ src/business/BusinessLogic.java
@@ -1,4 +1,15 @@
 package business;
 
+import dao.DataLayer;
+import util.CustomerTM;
+
+import java.util.List;
+
 public class BusinessLogic {
+    public static List<CustomerTM> getAllCustomers(){
+        return DataLayer.getAllCustomers();
+    }
+    public static boolean saveCustomer(String id, String name, String address){
+        return DataLayer.saveCustomers(new CustomerTM(id,name,address));
+    }
 }
